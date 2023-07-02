7e004b1c359940668f1d96a00dd082041c3e7e46
Phuc262002 <81422862+Phuc262002@users.noreply.github.com>
2023-07-02 13:32:55 +0700

    create migration

---
+++ database/migrations/2023_07_02_123956_create_products_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('sku', 50)->nullable();
+            $table->string('name', 200);
+            $table->string('description_summary', 200)->nullable();
+            $table->text('description_detail')->nullable();
+            $table->unsignedBigInteger('category_id')->default(1);
+            $table->integer('price')->default(0);
+            $table->integer('discount_price')->default(0);
+            $table->string('image_main', 200);
+            $table->integer('quantity')->default(0);
+            $table->integer('rating_count')->default(0);
+            $table->integer('average_rating')->default(0);
+            $table->unsignedBigInteger('brand_id');
+            $table->boolean('status')->default(true);
+            $table->timestamps();
+
+            $table->foreign('category_id')->references('id')->on('categories');
+            $table->foreign('brand_id')->references('id')->on('brands');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
+
---
+++ database/migrations/2023_07_02_124537_create_book_services_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBookServicesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('book_services', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('service_id');
+            $table->string('pet_name', 50);
+            $table->date('book_date');
+            $table->time('book_time');
+            $table->string('description', 200)->nullable();
+            $table->string('admin_note', 200)->nullable();
+            $table->enum('status', ['PENDING', 'HOLDING', 'ACCEPTED', 'COMPLETED', 'CANCELED'])->default('PENDING');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('service_id')->references('id')->on('services');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('book_services');
+    }
+}
