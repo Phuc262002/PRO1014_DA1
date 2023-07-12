92c619977e98f2e72b4f2364cf527a0a7c6237df
TrungCodeChoi <trungtqps26857@fpt.edu.vn>
2023-07-12 15:59:31 +0700

    update

--- resources/views/pages/client/account.blade.php
+++ resources/views/pages/client/account.blade.php
@@ -1,18 +1,10 @@
 @extends('layouts.client.master')
 
-@section('content')
-
-<!-- Header Section Start -->
-<div class="header section">
-
-    
+@section('content')  
     <!-- Header Bottom Start -->
     <div class="header-bottom">
         <div class="header-sticky">
-            <div class="container">
-                <div class="row align-items-center position-relative">
-
-                    
+            <div class="container">             
                     <div class="col-lg-3 col-md-8 col-6">
                         <div class="header-actions">                           
                             <!-- Header Action Button Start -->
@@ -122,11 +114,8 @@
 
                 </div>
             </div>
-        </div>
     </div>
     <!-- Header Bottom End -->
-
-</div>
 <!-- Header Section End -->
 
 <!-- Breadcrumb Area Start -->
@@ -135,10 +124,10 @@
         <div class="row">
             <div class="col-12 text-center">
                 <div class="breadcrumb-wrapper">
-                    <h2 class="breadcrumb-title">My Account</h2>
+                    <h2 class="breadcrumb-title">Tài khoản</h2>
                     <ul>
-                        <li><a href="index.html">Home</a></li>
-                        <li>My Account</li>
+                        <li><a href="index.html">Trang chủ</a></li>
+                        <li>Tài khoản của tôi</li>
                     </ul>
                 </div>
             </div>
@@ -161,11 +150,10 @@
                         <!-- My Account Tab Menu Start -->
                         <div class="col-lg-3 col-md-4">
                             <div class="myaccount-tab-menu nav" role="tablist">
-                                <a href="#account-info" data-bs-toggle="tab"><i class="fa fa-user"></i> Account Details</a>
-                                <a href="#orders" data-bs-toggle="tab"><i class="fa fa-cart-arrow-down"></i> Orders</a>
-                                <a href="#download" data-bs-toggle="tab"><i class="fa fa-cloud-download"></i> Download</a>
-                                <a href="#address-edit" data-bs-toggle="tab"><i class="fa fa-map-marker"></i> address</a>
-                                <a href="login.html"><i class="fa fa-sign-out"></i> Logout</a>
+                                <a href="#account-info" data-bs-toggle="tab"><i class="fa fa-user"></i> Tài khoản</a>
+                                <a href="#orders" data-bs-toggle="tab"><i class="fa fa-cart-arrow-down"></i> Đặt hàng</a>
+                                <a href="#address-edit" data-bs-toggle="tab"><i class="fa fa-map-marker"></i> Địa chỉ</a>
+                                <a href="login.html"><i class="fa fa-sign-out"></i> Đăng xuất</a>
                             </div>
                         </div>
                         <!-- My Account Tab Menu End -->
@@ -181,39 +169,39 @@
                                 <!-- Single Tab Content Start -->
                                 <div class="tab-pane fade" id="orders" role="tabpanel">
                                     <div class="myaccount-content">
-                                        <h3 class="title">Orders</h3>
+                                        <h3 class="title">Đơn hàng</h3>
                                         <div class="myaccount-table table-responsive text-center">
                                             <table class="table table-bordered">
                                                 <thead class="thead-light">
                                                     <tr>
-                                                        <th>Order</th>
-                                                        <th>Date</th>
-                                                        <th>Status</th>
-                                                        <th>Total</th>
-                                                        <th>Action</th>
+                                                        <th>Đơn hàng</th>
+                                                        <th>Ngày</th>
+                                                        <th>Trạng thái</th>
+                                                        <th>Tổng</th>
+                                                        <th>Hành động</th>
                                                     </tr>
                                                 </thead>
                                                 <tbody>
                                                     <tr>
                                                         <td>1</td>
-                                                        <td>Aug 22, 2018</td>
+                                                        <td> 22.02.2018</td>
                                                         <td>Pending</td>
                                                         <td>$3000</td>
-                                                        <td><a href="cart.html" class="btn btn btn-dark btn-hover-primary btn-sm rounded-0">View</a></td>
+                                                        <td><a href="cart.html" class="btn btn btn-dark btn-hover-primary btn-sm rounded-0">Xem</a></td>
                                                     </tr>
                                                     <tr>
                                                         <td>2</td>
-                                                        <td>July 22, 2018</td>
+                                                        <td>22.7.2018</td>
                                                         <td>Approved</td>
                                                         <td>$200</td>
-                                                        <td><a href="cart.html" class="btn btn btn-dark btn-hover-primary btn-sm rounded-0">View</a></td>
+                                                        <td><a href="cart.html" class="btn btn btn-dark btn-hover-primary btn-sm rounded-0">Xem</a></td>
                                                     </tr>
                                                     <tr>
                                                         <td>3</td>
-                                                        <td>June 12, 2019</td>
+                                                        <td>12.9.2019</td>
                                                         <td>On Hold</td>
                                                         <td>$990</td>
-                                                        <td><a href="cart.html" class="btn btn btn-dark btn-hover-primary btn-sm rounded-0">View</a></td>
+                                                        <td><a href="cart.html" class="btn btn btn-dark btn-hover-primary btn-sm rounded-0">Xem</a></td>
                                                     </tr>
                                                 </tbody>
                                             </table>
@@ -223,58 +211,20 @@
                                 <!-- Single Tab Content End -->
 
                                 <!-- Single Tab Content Start -->
-                                <div class="tab-pane fade" id="download" role="tabpanel">
-                                    <div class="myaccount-content">
-                                        <h3 class="title">Downloads</h3>
-                                        <div class="myaccount-table table-responsive text-center">
-                                            <table class="table table-bordered">
-                                                <thead class="thead-light">
-                                                    <tr>
-                                                        <th>Product</th>
-                                                        <th>Date</th>
-                                                        <th>Expire</th>
-                                                        <th>Download</th>
-                                                    </tr>
-                                                </thead>
-                                                <tbody>
-                                                    <tr>
-                                                        <td>Haven - Free Real Estate PSD Template</td>
-                                                        <td>Aug 22, 2018</td>
-                                                        <td>Yes</td>
-                                                        <td><a href="#" class="btn btn btn-dark btn-hover-primary rounded-0"><i class="fa fa-cloud-download m-r-5"></i> Download File</a></td>
-                                                    </tr>
-                                                    <tr>
-                                                        <td>TechWorld - Profolio Business Template</td>
-                                                        <td>Sep 12, 2018</td>
-                                                        <td>Never</td>
-                                                        <td><a href="#" class="btn btn btn-dark btn-hover-primary rounded-0"><i class="fa fa-cloud-download m-r-5"></i> Download File</a></td>
-                                                    </tr>
-                                                </tbody>
-                                            </table>
-                                        </div>
-                                    </div>
-                                </div>
+                                
                                 <!-- Single Tab Content End -->
-
-                                <!-- Single Tab Content Start -->
-                                <div class="tab-pane fade" id="payment-method" role="tabpanel">
-                                    <div class="myaccount-content">
-                                        <h3 class="title">Payment Method</h3>
-                                        <p class="saved-message">You Can't Saved Your Payment Method yet.</p>
-                                    </div>
-                                </div>
                                 <!-- Single Tab Content End -->
 
                                 <!-- Single Tab Content Start -->
                                 <div class="tab-pane fade" id="address-edit" role="tabpanel">
                                     <div class="myaccount-content">
-                                        <h3 class="title">Billing Address</h3>
+                                        <h3 class="title">Địa chỉ thanh toán</h3>
                                         <address>
                                             <p><strong>Alex Aya</strong></p>
                                             <p>1234 Market ##, Suite 900 <br>Lorem Ipsum, ## 12345</p>
-                                            <p>Mobile: (123) 123-456789</p>
+                                            <p>Số điện thoại: (123) 123-456789</p>
                                         </address>
-                                        <a href="#" class="btn btn btn-dark btn-hover-primary rounded-0"><i class="fa fa-edit m-r-10"></i>Edit Address</a>
+                                        <a href="#" class="btn btn btn-dark btn-hover-primary rounded-0"><i class="fa fa-edit m-r-10"></i>Chỉnh sửa địa chỉ</a>
                                     </div>
                                 </div>
                                 <!-- Single Tab Content End -->
@@ -282,54 +232,54 @@
                                 <!-- Single Tab Content Start -->
                                 <div class="tab-pane fade" id="account-info" role="tabpanel">
                                     <div class="myaccount-content">
-                                        <h3 class="title">Account Details</h3>
+                                        <h3 class="title">Tài khoản</h3>
                                         <div class="account-details-form">
                                             <form action="#">
                                                 <div class="row">
                                                     <div class="col-lg-6">
                                                         <div class="single-input-item m-b-15">
-                                                            <label for="first-name" class="required m-b-10">First Name</label>
-                                                            <input type="text" id="first-name" placeholder="First Name" />
+                                                            <label for="first-name" class="required m-b-10">Tên</label>
+                                                            <input type="text" id="first-name" placeholder="Tên" />
                                                         </div>
                                                     </div>
                                                     <div class="col-lg-6">
                                                         <div class="single-input-item m-b-15">
-                                                            <label for="last-name" class="required m-b-10">Last Name</label>
-                                                            <input type="text" id="last-name" placeholder="Last Name" />
+                                                            <label for="last-name" class="required m-b-10">Họ</label>
+                                                            <input type="text" id="last-name" placeholder="Họ" />
                                                         </div>
                                                     </div>
                                                 </div>
                                                 <div class="single-input-item m-b-15">
-                                                    <label for="display-name" class="required m-b-10">Display Name</label>
-                                                    <input type="text" id="display-name" placeholder="Display Name" />
+                                                    <label for="display-name" class="required m-b-10">Tên hiển thị</label>
+                                                    <input type="text" id="display-name" placeholder="Tên hiển thị" />
                                                 </div>
                                                 <div class="single-input-item m-b-15">
-                                                    <label for="email" class="required m-b-5">Email Addres</label>
-                                                    <input type="email" id="email" placeholder="Email Address" />
+                                                    <label for="email" class="required m-b-5">Email</label>
+                                                    <input type="email" id="email" placeholder="Email" />
                                                 </div>
                                                 <fieldset>
-                                                    <legend>Password change</legend>
+                                                    <legend>Đổi mật khẩu</legend>
                                                     <div class="single-input-item m-b-15">
-                                                        <label for="current-pwd" class="required m-b-10">Current Password</label>
-                                                        <input type="password" id="current-pwd" placeholder="Current Password" />
+                                                        <label for="current-pwd" class="required m-b-10">Mật khẩu hiển thị</label>
+                                                        <input type="password" id="current-pwd" placeholder="Mật khẩu hiển thị" />
                                                     </div>
                                                     <div class="row m-b-n15">
                                                         <div class="col-lg-6">
                                                             <div class="single-input-item m-b-15">
-                                                                <label for="new-pwd" class="required m-b-10">New Password</label>
-                                                                <input type="password" id="new-pwd" placeholder="New Password" />
+                                                                <label for="new-pwd" class="required m-b-10">Mật khẩu mới</label>
+                                                                <input type="password" id="new-pwd" placeholder="Mật khẩu mới" />
                                                             </div>
                                                         </div>
                                                         <div class="col-lg-6">
                                                             <div class="single-input-item m-b-15">
-                                                                <label for="confirm-pwd" class="required m-b-10">Confirm Password</label>
-                                                                <input type="password" id="confirm-pwd" placeholder="Confirm Password" />
+                                                                <label for="confirm-pwd" class="required m-b-10">Nhập lại mật khẩu</label>
+                                                                <input type="password" id="confirm-pwd" placeholder="Nhập lại mật khẩu" />
                                                             </div>
                                                         </div>
                                                     </div>
                                                 </fieldset>
                                                 <div class="single-input-item single-item-button m-t-30">
-                                                    <button class="btn btn btn-primary btn-hover-dark rounded-0">Save Changes</button>
+                                                    <button class="btn btn btn-primary btn-hover-dark rounded-0">Lưu thay đổi</button>
                                                 </div>
                                             </form>
                                         </div>
