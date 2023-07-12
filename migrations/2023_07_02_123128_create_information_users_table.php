00c9bc2626b50bc27a9303b8cc8bc95dbb3058d4
Nhan2095 <nhanntps26577@fpt.edu.vn>
2023-07-12 09:38:02 +0700

    f

---
+++ migrations/2023_07_02_123128_create_information_users_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateInformationUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('information_users', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('fullname', 50);
+            $table->string('phone', 15);
+            $table->string('address', 50);
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('information_users');
+    }
+}
