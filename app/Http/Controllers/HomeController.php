d0091b4efbac9a061ae6e98a42694b54f66f6531
Nhan2095 <nhanntps26577@fpt.edu.vn>
2023-07-13 15:20:31 +0700

    update

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,17 +4,37 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Product;
+use App\Models\Banner;
+
 
 class HomeController extends Controller
 {
     public function index()
     {
         $title="Pets Care";
-        $products = Product::where('status', true)
+        $topnew = Product::where('status', true)
                         ->orderBy('created_at', 'desc')
                         ->take(8)
                         ->get();
-        dd($products);
-        // return view('home', compact('title'));
+        // dd($products);
+        
+
+        $topsale = Product::where('status', true)
+                        ->where('discount_price', '>', 0)
+                        ->orderBy('created_at', 'desc')
+                        ->take(8)
+                        ->get();
+        // dd($products);
+        
+
+        $banner = Banner::where([
+                                    'status' => true,
+                                    'banner_type' => 'SLIDER'
+                                ])
+                                                
+                        ->orderBy('created_at', 'desc')
+                        ->get();
+
+        return view('home', compact('title', 'topnew', 'topsale', 'banner'));
     }
 }
