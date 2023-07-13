87129c608acb36fa2ef48737cb2209cb64bfd7c8
khuong03 <khuongttaps26697@fpt.edu.vn>
2023-07-13 13:30:01 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ use App\Http\Controllers\DashboardController;
 |
 */
 
-Route::get('/', [HomeController::class,'index'])->name('home');
+Route::get('/', [HomeController::class, 'index'])->name('home');
 
 Route::get('shop', function () {
     return view('pages.client.shop');
@@ -96,15 +96,8 @@ Route::get('test', function () {
     return view('test');
 });
 
-<<<<<<< HEAD
-Route::group(['prefix' => 'admin'], function () {
-    Route::get('/', function () {
-        return view('pages.admin.dashboard');
-    })->name('admin.dashboard');
-=======
 Route::group(['prefix' => 'admin', 'middleware' => ['auth', 'admin']], function () {
     Route::get('/', [DashboardController::class, 'index'])->name('admin.dashboard');
->>>>>>> d411336c09f799de104a2a1bf5dca2b5c4567c11
     Route::get('quan-ly-tai-khoan', function () {
         return view('pages.admin.product_manager');
     })->name('admin.product_manager');
