3512339f94561cf81ba721f7ee464d071af9550a
Phuc262002 <81422862+Phuc262002@users.noreply.github.com>
2023-07-13 12:50:04 +0700

    update

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,12 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Product;
 
 class HomeController extends Controller
 {
     public function index()
     {
         $title="Pets Care";
-        return view('home', compact('title'));
+        $products = Product::where('status', true)
+                        ->orderBy('created_at', 'desc')
+                        ->take(8)
+                        ->get();
+        dd($products);
+        // return view('home', compact('title'));
     }
 }
