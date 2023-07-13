45a0d9fc3bd27b37495752cd9a13bd5ddb51de78
khuong03 <khuongttaps26697@fpt.edu.vn>
2023-07-13 13:47:00 +0700

    update

--- resources/views/layouts/admin/inc/header.blade.php
+++ resources/views/layouts/admin/inc/header.blade.php
@@ -78,7 +78,7 @@
                         <!-- App Search-->
                         <form class="app-search d-none d-md-block">
                             <div class="position-relative">
-                                <input type="text" class="form-control" placeholder="Search..." autocomplete="off"
+                                <input type="text" class="form-control" placeholder="Tìm kiếm" autocomplete="off"
                                     id="search-options" value="" />
                                 <span class="mdi mdi-magnify search-widget-icon"></span>
                                 <span class="mdi mdi-close-circle search-widget-icon search-widget-icon-close d-none"
@@ -189,7 +189,7 @@
                                 <form class="p-3">
                                     <div class="form-group m-0">
                                         <div class="input-group">
-                                            <input type="text" class="form-control" placeholder="Search ..."
+                                            <input type="text" class="form-control" placeholder="Tìm kiếm"
                                                 aria-label="Recipient's username" />
                                             <button class="btn btn-primary" type="submit">
                                                 <i class="mdi mdi-magnify"></i>
@@ -574,11 +574,11 @@
                             <button type="button" class="btn shadow-none" id="page-header-user-dropdown"
                                 data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                 <span class="d-flex align-items-center">
-                                    <img class="rounded-circle header-profile-user"
-                                        src="{{Auth::user()->image}}" alt="Header Avatar" />
+                                    <img class="rounded-circle header-profile-user" src="{{ Auth::user()->image }}"
+                                        alt="Header Avatar" />
                                     <span class="text-start ms-xl-2">
                                         <span class="d-none d-xl-inline-block ms-1 fw-medium user-name-text">
-                                            {{Auth::user()->name}}
+                                            {{ Auth::user()->name }}
                                         </span>
                                         <span
                                             class="d-none d-xl-block ms-1 fs-12 text-muted user-name-sub-text">Admin</span>
@@ -587,7 +587,7 @@
                             </button>
                             <div class="dropdown-menu dropdown-menu-end">
                                 <!-- item-->
-                                <h6 class="dropdown-header">Chào {{Auth::user()->name}}!</h6>
+                                <h6 class="dropdown-header">Chào {{ Auth::user()->name }}!</h6>
                                 <a class="dropdown-item" href="pages-profile.html"><i
                                         class="mdi mdi-account-circle text-muted fs-16 align-middle me-1"></i>
                                     <span class="align-middle">Profile</span></a>
