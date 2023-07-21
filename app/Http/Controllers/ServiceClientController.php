bbfcfda5a14633c33cb4c15a7f77bf1392567807
Phuc262002 <81422862+Phuc262002@users.noreply.github.com>
2023-07-21 11:01:07 +0700

    update

--- app/Http/Controllers/ServiceClientController.php
+++ app/Http/Controllers/ServiceClientController.php
@@ -39,7 +39,7 @@ class ServiceClientController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(Product $product)
+    public function show(Service $product)
     {
         //
     }
@@ -47,7 +47,7 @@ class ServiceClientController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(Product $product)
+    public function edit(Service $product)
     {
         //
     }
@@ -55,7 +55,7 @@ class ServiceClientController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Product $product)
+    public function update(Request $request, Service $product)
     {
         //
     }
@@ -63,7 +63,7 @@ class ServiceClientController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Product $product)
+    public function destroy(Service $product)
     {
         //
     }
